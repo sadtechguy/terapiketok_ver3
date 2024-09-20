eb49cca30906273ccb69d1fa37166d81e9adbab3
Martin <martin.techguy@outlook.com>
2024-09-20 16:22:53 +0700

    add templates home page

--- terapiketok/routes/home.py
+++ terapiketok/routes/home.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template_string, render_template
 
-home_bp = Blueprint('home', __name__, template_folder="#")
+home_bp = Blueprint('home', __name__, template_folder="../templates/home")
 
 @home_bp.route('/')
 @home_bp.route('/home')
 def home_page():
-    return render_template_string("<h1>Hello, world!</H1>")
+    return render_template('home.html')
