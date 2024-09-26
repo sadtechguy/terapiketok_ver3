d00ad636a96b66952db9a0f8a299aa5cb4ee169f
Martin <martin.techguy@outlook.com>
2024-09-26 11:54:23 +0700

    add different background color for pending and close batches

--- terapiketok/routes/home.py
+++ terapiketok/routes/home.py
@@ -8,12 +8,22 @@ home_bp = Blueprint('home', __name__, template_folder="../templates/home")
 def home_page():
     batches = [(datetime.date(2024, 10, 23), 'Rabu', 1, 'Sesi 1', datetime.time(11, 0), datetime.time(13, 0), 20, 0, 'PENDING'), (datetime.date(2024, 10, 22), 'Selasa', 2, 'Sesi 2', datetime.time(15, 0), datetime.time(17, 0), 20, 0, 'OPEN'), (datetime.date(2024, 10, 22), 'Selasa', 1, 'Sesi 1', datetime.time(11, 0), datetime.time(15, 0), 20, 0, 'OPEN')]
     clean_batches = []
-    for b_date, b_day, s_id, s_name, b_start, b_end, limit, available, status in batches:
-        if status == "OPEN":
-            bgcolor = 'success'
-        else:
-            bgcolor = "danger"
-        clean_batches.append((b_date.strftime('%d-%m-%Y'), b_day, s_id, s_name, b_start.strftime('%H:%M'), b_end.strftime('%H:%M'), status, bgcolor))
+    for batch in batches:
+        batch_date = batch[0].strftime('%d-%m-%Y')
+        batch_day = batch[1]
+        schedule_id = batch[2]
+        schedule_name = batch[3]
+        start_hour = batch[4]
+        end_hour = batch[5]
+        status = batch[8]
 
+        left_side_class = "bg-dark" if status == "OPEN" else "black-transparent"
+        middle_side_class = "text-dark" if status =="OPEN" else "white-transparent"
+        right_side_class = "success" if status == "OPEN" else "danger"
+
+        clean_batches.append(
+            (batch_date, batch_day, schedule_id, schedule_name, start_hour, end_hour, status,
+              left_side_class, middle_side_class, right_side_class)
+        )
 
     return render_template('home.html', batches=clean_batches)
