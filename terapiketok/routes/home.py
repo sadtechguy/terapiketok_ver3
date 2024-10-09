745c0b9b2d18eb309c7887c12cfdfa9a26155d3a
Martin <martin.techguy@outlook.com>
2024-10-09 16:43:38 +0700

    use session instead of passing data using url_for

--- terapiketok/routes/home.py
+++ terapiketok/routes/home.py
@@ -1,12 +1,12 @@
 import datetime
-from flask import Blueprint, render_template_string, render_template, request, redirect, url_for
+from flask import Blueprint, render_template_string, render_template, request, redirect, url_for, session
 from ..services.database import fetch_available_batch
 from ..services.data_processing import authenticate_user
 from ..forms import LoginForm
 
 home_bp = Blueprint('home', __name__, template_folder="../templates/home")
 
-@home_bp.route('/')
+@home_bp.route('/', methods=['GET', 'POST'])
 @home_bp.route('/home', methods=['GET', 'POST'])
 def home_page():
     text_header = "Harap masukkan nama dan nomor HP"
@@ -21,13 +21,15 @@ def home_page():
 
         if authenticate_user(username, phone):
             # Redirect to the home page after successful login
-            return redirect(url_for('.register_page', username=username, phone=phone))
+            session["username"] = username
+            session["phone"] = phone
+            return redirect(url_for('.register_page'))
     return render_template('home.html', form=form, text_header=text_header)
 
-@home_bp.route('/register')
+@home_bp.route('/register', methods=['GET', 'POST'])
 def register_page():
-    username = request.args.get('username')
-    phone = request.args.get('phone')
+    username = session.get("username")
+    phone = session.get("phone")
 
     if authenticate_user(username, phone) == False:
         return redirect(url_for('.home_page'))
@@ -44,9 +46,11 @@ def register_page():
         end_hour = batch[5]
         status = batch[8]
 
-        left_side_class = "bgActive" if status == "OPEN" else "bgNonActive"
-        middle_side_class = "text-dark" if status =="OPEN" else "white-transparent"
-        right_side_class = "success" if status == "OPEN" else "danger"
+        classes = {
+            "OPEN": ("bgActive", "text-dark", "success"),
+            "CLOSED": ("bgNonActive", "white-transparent", "danger")
+        }
+        left_side_class, middle_side_class, right_side_class = classes.get(status, ("bgNonActive", "white-transparent", "danger"))
 
         clean_batches.append(
             (batch_date, batch_day, schedule_id, schedule_name, start_hour, end_hour, status,
