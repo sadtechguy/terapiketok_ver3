e71fc25b5a2349467f3e68b35fbc4f913a8a559d
Martin <martin.techguy@outlook.com>
2024-11-06 10:14:14 +0700

    add form for default and change color of flash in login manager using css

--- terapiketok/__init__.py
+++ terapiketok/__init__.py
@@ -17,7 +17,7 @@ if ENV == "DEV":
     app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost:5432/{DATABASE_NAME}"
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     app.config["SESSION_PERMANENT"] = True
-    app.config["PERMANENT_SESSION_LIFETIME"] = 60
+    app.config["PERMANENT_SESSION_LIFETIME"] = 1800
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ""
